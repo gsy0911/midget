807aa00574676fe6f56413aa8617f61f34a9214e
yoshiki <yoshiki0911@gmail.com>
2021-06-13 18:14:54 +0900

    #5 chore: remove unused lines

--- src/core/ICore.ts
+++ src/core/ICore.ts
@@ -1,11 +1,4 @@
-import { ITask, ISetting } from "../states";
-
 export interface ICore {
-	loadTaskList: () => Promise<ITask[]>;
-	saveTask: (task: ITask) => Promise<ITask[]>;
-	deleteTask: (taskId: string) => Promise<ITask[]>;
-	loadSetting: () => Promise<ISetting>;
-	saveSetting: (setting: ISetting) => Promise<ISetting>;
 }
 
 // add core-definition to window-object
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,1 @@
 import { combineReducers, createStore } from "redux";
-import { IState } from "./states";
-
-// const combinedReducer = combineReducers<IState>({
-// 	taskList: taskReducer,
-// });
-//
-// export const store = createStore(combinedReducer);
