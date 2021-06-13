855b87ae93919d472534d3c1845ab6eb8572004b
yoshiki <yoshiki0911@gmail.com>
2021-06-13 16:59:24 +0900

    #3 modify timer

--- src/modules/NeonSchoolClock.tsx
+++ src/modules/NeonSchoolClock.tsx
@@ -1,9 +1,43 @@
 import React, {useState, useEffect} from 'react';
-import {Theme, makeStyles} from '@material-ui/core/styles';
-import {NeonBox, NeonBoxProps} from './NeonBox';
+import {NeonBox} from './NeonBox';
 
 
-export interface NeonSchoolClockProps extends Partial<NeonBoxProps> {
+export interface ModeProps {
+	durationMinute: number
+	color: string
+	title: string
+	next: string
+}
+
+export interface TimetableProps {
+	modes: { [modeName: string]: ModeProps },
+	initial: string,
+}
+
+const workMode: ModeProps = {
+	durationMinute: 10,
+	color: "cyan",
+	title: "WORK NOW",
+	next: "break"
+}
+
+const breakMode: ModeProps = {
+	durationMinute: 1,
+	color: "orange",
+	title: "REST",
+	next: "work"
+}
+
+const defaultTimeTable: TimetableProps = {
+	modes: {
+		work: workMode,
+		break: breakMode
+	},
+	initial: "work"
+}
+
+export interface NeonSchoolClockProps {
+	timetable?: TimetableProps
 }
 
 const dateToString = (date: Date): string => {
@@ -21,21 +55,43 @@ const dateToString = (date: Date): string => {
 }
 
 export const NeonSchoolClock: React.FC<NeonSchoolClockProps> = (props) => {
-	const [date, setDate] = useState(new Date())
+	const {modes, initial} = props.timetable || defaultTimeTable
+	const initialMode = modes[initial]
+	// states
+	const [date, setDate] = useState<Date>(new Date())
+	const [mode, setMode] = useState<ModeProps>(initialMode)
+	const [count, setCount] = useState<number>(initialMode.durationMinute * 60)
+	const [loopCount, setLoopCount] = useState<number>(1)
 
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setDate(() => new Date());
-		}, 1000 * 10);
+		}, 1000);
 		return () => clearInterval(interval);
 	}, []);
 
+	// ticks 1[sec]
+	useEffect(() => {
+		setCount(count => count - 1)
+		if (count <= 0) {
+			setMode(mode => modes[mode.next])
+			if (mode.next === initial) {
+				setLoopCount(loopCount => loopCount + 1)
+			}
+		}
+	}, [date])
+
+	// check loops
+	useEffect(() => {
+		setCount(mode.durationMinute * 60)
+	}, [mode])
+
 	return (
 		<NeonBox
-			header={"hello"}
-			title={"WORK NOW"}
+			header={`Loop: ${loopCount}`}
+			title={mode.title}
 			subtitle={`${dateToString(date)} JST`}
-			color={"cyan"}
+			color={mode.color}
 		/>
 	)
 }
