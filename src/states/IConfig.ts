10a708fbbfa1e622d5cb0f94bb137fefa3fd7a91
yoshiki <yoshiki0911@gmail.com>
2021-06-19 10:34:06 +0900

    #18 modify config

--- src/states/IConfig.ts
+++ src/states/IConfig.ts
@@ -1,4 +1,4 @@
-import {TimetableProps, defaultTimeTable} from "./ITimetable";
+import {ModeProps, workMode, breakMode, TimetableProps, defaultTimeTable} from "./ITimetable";
 
 export interface IConfigFile {
 	windowSize: "small" | "medium" | "large"
@@ -16,11 +16,17 @@ export const defaultConfigFile: IConfigFile = {
 export interface IApplicationState {
 	windowSize: "small" | "medium" | "large"
 	timetable: TimetableProps
+	currentMode: ModeProps
+	modeUntil: number
+	nextMode: ModeProps
 	workingAt: string
 }
 
 export const defaultApplicationState: IApplicationState = {
 	windowSize: "medium",
 	timetable: defaultTimeTable,
+	currentMode: workMode,
+	modeUntil: 0,
+	nextMode: breakMode,
 	workingAt: "companyA"
 }
